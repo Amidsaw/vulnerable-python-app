3aadbb81a2487d3952695b02c84cafcaf24e9a31
httpnotonly <httpnotonly@gmail.com>
2022-01-20 17:11:29 +0000

    Update hardcoded.py

--- hardcoded.py
+++ hardcoded.py
@@ -1,5 +1,6 @@
 import requests
 
 google_key = "AIzaSyCaEAG3n587vn4m5a2zllpuURIzGJA2h4uyf73"
+google_key_2 = "AIzaSyCaEAG3n587vn4m5a2zllpuURIzGJA2h4uyf74"
 
 response = requests.get(f'https://any.thing/?key={google_key}').json()
